a24367149935f7cfe185ea782287ee533497bc3b
Denis Bobrov <delneg@yandex.ru>
2021-12-15 20:42:50 +0300

    First working version

--- Benchmark/Benchmark.swift
+++ Benchmark/Benchmark.swift
@@ -4,10 +4,14 @@
 
 import XCTest
 import Nuke
-import Alamofire
-import AlamofireImage
-import Kingfisher
+import NukeAVIFPlugin
+
+//import Alamofire
+//import AlamofireImage
+//import Kingfisher
 import SDWebImage
+import SDWebImageAVIFCoder
+
 //import AppleSample
 
 // MARK: - Main-Thread Performance
@@ -23,11 +27,15 @@ class CustomImageView: UIImageView {
 /// fetched image is availalbe in the memory cache (cache hit).
 class CacheHitPerformanceTests: XCTestCase {
     let view = CustomImageView()
-    let image = UIImage(named: "fixture")! // same image so that it gets decoded once
+    let image =   UIImage(named: "fixture")! // same image so that it gets decoded once
     // 10_000 iterations, but 100 unique URLs
-    let urls: [URL] = (0..<25_000).map { _ in URL(string: "https://test.com/\(arc4random_uniform(100)).jpeg")! }
+    let urls: [URL] = ["https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639499987438-38d2bc8a6b79?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637840127377-55281ff01927?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638736879218-785eda895f15?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637925897919-81440462a54f?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638538002369-55d57aac2178?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639428133972-8dceaed37f13?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1636990536161-710184e1047f?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639125470955-faa8f6fef1ab?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637534371564-458a3a29972f?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637248761448-a3b0733a304d?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637298229923-f341a84d21b8?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637009749350-fed93f5b9f4d?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638349978185-b2556ec8436a?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638553966969-688dfd8d624e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639414778048-9b4a9abd4895?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1636955779321-819753cd1741?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637249876871-b1a93c13ed1e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400"]
+        .map { x in URL(string:x)! }
+//    let urls: [URL] = (0..<25_000).map { _ in URL(string:"https://images.unsplash.com/photo-1622977265144-83e92cf7679b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400")! }
 
     func testNuke() {
+        AVIFImageDecoder.enable()
+
         for url in urls {
             Nuke.ImageCache.shared[url] = ImageContainer(image: image)
         }
@@ -39,31 +47,9 @@ class CacheHitPerformanceTests: XCTestCase {
         }
     }
 
-    func testAlamofireImage() {
-        for url in urls {
-            UIImageView.af.sharedImageDownloader.imageCache?.add(image, for: URLRequest(url: url), withIdentifier: nil)
-        }
-
-        measure {
-            for url in urls {
-                view.af.setImage(withURL: url)
-            }
-        }
-    }
-
-    func testKingfisher() {
-        for url in urls {
-            KingfisherManager.shared.cache.store(image, original: nil, forKey: url.absoluteString, processorIdentifier: "", cacheSerializer: DefaultCacheSerializer.default, toDisk: false, completionHandler: nil)
-        }
-
-        measure {
-            for url in urls {
-                view.kf.setImage(with: url)
-            }
-        }
-    }
-
     func testSDWebImage() {
+        let AVIFCoder = SDImageAVIFCoder.shared
+        SDImageCodersManager.shared.addCoder(AVIFCoder)
         for url in urls {
             SDImageCache.shared.store(image, imageData: nil, forKey: url.absoluteString, toDisk: false, completion: nil)
         }
@@ -74,29 +60,6 @@ class CacheHitPerformanceTests: XCTestCase {
             }
         }
     }
-
-//    func testAppleSample() {
-//        let urls = self.urls.map { $0 as NSURL }
-//
-//        for url in urls {
-//            AppleSample.ImageCache.publicCache.storeImage(image, for: url)
-//        }
-//
-//        let items = urls.map {
-//            Item(image: nil, url: NSURL(string: $0.absoluteString!)!)
-//        }
-//
-//        let allItems = zip(urls, items)
-//
-//        measure {
-//            for (url, item) in allItems {
-//                AppleSample.ImageCache.publicCache.load(url: url, item: item) { item, image in
-//                    // Techincally we update the item in this callback and reload
-//                    // the view that is currenltly displaying it.
-//                    self.view.image = image
-//                }
-//            }
-//        }
 //    }
 }
 
@@ -105,34 +68,22 @@ class CacheHitPerformanceTests: XCTestCase {
 /// to be sent (cache miss).
 class CacheMissPerformanceTests: XCTestCase {
     let view = CustomImageView()
-    let urls: [URL] = (0..<20_000).map { _ in URL(string: "https://test.com/\(arc4random()).jpeg")! }
+    let urls: [URL] = ["https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637102839236-daf3d361fa83?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637208469121-a5b8f2db869b?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637452313272-9f5855b5f2bc?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637913754840-3c8755479fdd?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638984498411-345388047a8e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638211374179-3f34b71605c9?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637082856736-2f80274bcaa5?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638173973774-286a6a69d197?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639499987438-38d2bc8a6b79?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637840127377-55281ff01927?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638736879218-785eda895f15?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637925897919-81440462a54f?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638538002369-55d57aac2178?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639428133972-8dceaed37f13?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1636990536161-710184e1047f?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639125470955-faa8f6fef1ab?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637534371564-458a3a29972f?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637248761448-a3b0733a304d?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637298229923-f341a84d21b8?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637009749350-fed93f5b9f4d?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638349978185-b2556ec8436a?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1638553966969-688dfd8d624e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1639414778048-9b4a9abd4895?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1636955779321-819753cd1741?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400", "https://images.unsplash.com/photo-1637249876871-b1a93c13ed1e?crop=entropy&fit=crop&fm=avif&h=400&q=80&w=400"]
+        .map { x in URL(string:x)! }
 
 
     func testNuke() {
+        
         measure {
             for url in urls {
                 Nuke.loadImage(with: url, into: view)
             }
         }
     }
-
-     func testAlamofireImage() {
-        measure {
-            for url in urls {
-                view.af.setImage(withURL: url)
-            }
-        }
-     }
-
-    func testKingfisher() {
-        measure {
-            for url in urls {
-                view.kf.setImage(with: url)
-            }
-        }
-    }
-
+    
     func testSDWebImage() {
+        let AVIFCoder = SDImageAVIFCoder.shared
+        SDImageCodersManager.shared.addCoder(AVIFCoder)
         measure {
             for url in urls {
                 view.sd_setImage(with: url)
---
+++ Podfile
@@ -0,0 +1,33 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Image Frameworks Benchmark' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # use_frameworks!
+  use_modular_headers!
+
+  # Pods for Image Frameworks Benchmark
+  pod 'Nuke', '~> 9.0'
+  # pod 'libavif/libaom'
+  pod 'libavif/libdav1d'
+  pod 'Nuke-AVIF-Plugin', :git => 'https://github.com/delneg/Nuke-AVIF-Plugin.git'
+  pod 'SDWebImage'
+  pod 'SDWebImageAVIFCoder'
+  # pod 'libavif'
+end
+
+
+target 'Benchmark' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # use_frameworks!
+  use_modular_headers!
+
+  # Pods for Image Frameworks Benchmark
+  pod 'Nuke', '~> 9.0'
+  # pod 'libavif/libaom'
+  pod 'libavif/libdav1d'
+  pod 'Nuke-AVIF-Plugin', :git => 'https://github.com/delneg/Nuke-AVIF-Plugin.git'
+  pod 'SDWebImage'
+  pod 'SDWebImageAVIFCoder'
+  # pod 'libavif'
+end
